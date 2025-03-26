fc28e37ab44f569a669f59cdd0acb8d1b1712334
KingChava1598 <salvador760177@gmail.com>
2025-03-25 22:37:28 -0500

    Added more pages, updated the homepage, added page navigations.

---
+++ eqm/lib/Pages/query.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class Query extends StatelessWidget 
+{
+  const Query({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: appBar(context),
+      body: Column
+      (
+        children:
+        [
+          Center(
+            child: ClipPath
+            (
+              clipper: DownTrapezoidClipper(),
+              child: Container
+              (
+                width: 300,
+                height: 30,
+                color: const Color.fromARGB(255, 127, 255, 131),
+                alignment: Alignment.center,
+                child: const Text
+                (
+                  'Query',
+                  style: TextStyle
+                  (
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )
+                )
+              )
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class DownTrapezoidClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Border.all
+    (
+      color: Colors.black,
+      width: 5,
+    );
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width * 0.15, size.height);
+    path.lineTo(size.width * 0.85, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
